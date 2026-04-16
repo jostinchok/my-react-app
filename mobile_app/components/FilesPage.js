37996f91adf80cb0963555bbb969960358061645
FZhen2450 <104400141@students.swinburne.edu.my>
2026-04-16 15:44:01 +0000

    update mobile_app

--- mobile_app/components/FilesPage.js
+++ mobile_app/components/FilesPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, Pressable, StyleSheet, Alert, Picker } from 'react-native';
+import { View, Text, ScrollView, Pressable, StyleSheet, Alert } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import * as DocumentPicker from 'expo-document-picker';
 
 const FilesPage = ({ t }) => {
