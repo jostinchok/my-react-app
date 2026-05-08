b75897c15a1b19da7b38be759fd562f95ef056c6
Fa Zhen NG <104400141@students.swinburne.edu.my>
2026-05-08 22:26:31 +0800

    Update UserPage

--- database/demo_canvas_courses.sql
+++ database/demo_canvas_courses.sql
@@ -22,6 +22,7 @@ CREATE TABLE IF NOT EXISTS users (
     role_id INT NULL,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE,
+    birthday DATE NULL,
     password_hash VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
